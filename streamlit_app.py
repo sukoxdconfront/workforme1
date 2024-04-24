b9f339f9d9329250770b58d281931de6095585c6
sukoxdconfront <167644774+sukoxdconfront@users.noreply.github.com>
2024-04-23 18:38:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,8 @@ import streamlit as st
 import subprocess
 import time
 
-# Run commands as sudo
-subprocess.run(["sudo", "apt", "update"], stdout=subprocess.PIPE)
-subprocess.run(["sudo", "apt", "install", "tmate", "-y"], stdout=subprocess.PIPE)
-
 # Start tmate
-subprocess.run(["sudo", "tmate"], stdout=subprocess.PIPE)
+subprocess.run(["tmate"], stdout=subprocess.PIPE)
 
 # Display a message to the user
 st.markdown("Tmate is now running. You can connect to it using the following command:")
