f812d1d56f524d7c95b64aca4e624aebdf09ad3b
sukoxdconfront <167644774+sukoxdconfront@users.noreply.github.com>
2024-04-23 18:32:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ if st.button("Install Tmate"):
     st.write("Installing tmate...")
 
     # Install tmate
-    st.command("sudo apt update")
-    st.command("sudo apt install tmate")
+    st.command("apt update")
+    st.command("apt install tmate")
 
     # Start tmate
-    st.command("sudo tmate")
+    st.command("tmate")
 
     st.write("Tmate installed and running!")
